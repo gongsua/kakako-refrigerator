f155e4b443dcfb1157a4151a6acaef558a8286eb
gongsua <thddbswls11@naver.com>
2025-02-26 11:21:17 +0900

    code upload

---
+++ backend/src/test/java/org/example/refrigerator/RefrigeratorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.refrigerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefrigeratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
